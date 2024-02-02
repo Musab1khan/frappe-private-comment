9a51228c88c0f6b7471e043eca9e78a8345cabb0
zeel-codder <zeelprajapati321@gmail.com>
2024-02-02 20:27:28 +0530

    feat: update visibility options values and enhance comment UI

--- comment_enhancer/__init__.py
+++ comment_enhancer/__init__.py
@@ -8,6 +8,8 @@ def patch_add_comments_in_timeline():
 
     from comment_enhancer.helpers.comment import add_comments_in_timeline
 
+    # A monkey patch was written for this function as it is used in many places within Frappe. Care was taken to avoid breaking existing code.
+
     frappe_load.add_comments = add_comments_in_timeline
 
 
